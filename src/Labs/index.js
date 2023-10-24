671bb4af98eb344451c2995e133b3c2d5d5d1592
Hunter Groff <huntergroff22@gmail.com>
2023-10-24 13:06:38 -0400

    fixing nav

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router";
 import Assignment3 from "./a3";
-import { Link } from "react-router-dom";
 import Nav from "../Nav";
 import Assignment4 from "./a4";
 import { Provider } from "react-redux";
@@ -11,8 +10,6 @@ function Labs() {
         <Provider store={store}>
        <div>
         <Nav />
-        <Link to="a3">A3</Link>
-        <Link to="a4">A4</Link>
         <Routes>
             <Route path="a3/*" element={<Assignment3 />} />
             <Route path="a4/*" element={<Assignment4 />} />
